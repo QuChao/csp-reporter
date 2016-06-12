b1949cc50cdaab1f2218b8ca7207ca5dcfb088e0
Chao QU <mail@quchao.com>
2016-06-12 22:45:02 +0100

    Add an encoding option to the Simple LogFormat plugin.
    # - Add an option to the sample config.
    # - Set default encoding to UTF-8.

--- cspreporter/plugins/logformats/simple.py
+++ cspreporter/plugins/logformats/simple.py
@@ -3,12 +3,13 @@ from cspreporter.core.plugins import LogFormat
 class Simple(LogFormat):
     title = 'Clean up log'
     desc = 'This plugin cleans up log entries to JSON data only'
+    enc = ''
 
     def setup(self):
-        pass
+        self.enc = self.config.get('plugins.logformats.simple', 'encoding')
 
     def process(self, s):
-        tmp = s.decode()
+        tmp = s.decode(self.enc)
         return tmp[tmp.rfind('{"csp-report"'):]
 
     def teardown(self):
